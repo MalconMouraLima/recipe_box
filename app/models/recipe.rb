62a889ac930940a5baa1b5bb96acabb137203fc7
MalconMouraLima <malcon.moura.lima@gmail.com>
2017-04-19 01:03:32 -0300

    20170419_0103

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,12 +4,11 @@ class Recipe < ApplicationRecord
   has_many :directions
 
   accepts_nested_attributes_for :ingredients,
-                                reject_if: proc { |attributes| attributes['name']}.blank? },
-                                allow_destroy: true
-
-  accepts_nested_attributes_for :directions,
-                                reject_if: proc { |attributes| attributes['step']}.blank? },
-                                allow_destroy: true
+    															reject_if: proc { |attributes| attributes['name'].blank? },
+    															allow_destroy: true
+   	accepts_nested_attributes_for :directions,
+    															reject_if: proc { |attributes| attributes['step'].blank? },
+    															allow_destroy: true
 
   validates :title, :description, :image, presence: true
   has_attached_file :image, styles: { medium: "400x400#>" }
