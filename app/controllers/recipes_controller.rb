f86323fc7d2b4b20d6df586361c18435229b060e
MalconMouraLima <malcon.moura.lima@gmail.com>
2017-04-20 09:48:40 -0300

    20170420_0948

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,19 +1,19 @@
 class RecipesController < ApplicationController
-  before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+	before_action :find_recipe, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @recipe = Recipe.all.order("created_at DESC")
-  end
+	def index
+		@recipe = Recipe.all.order("created_at DESC")
+	end
 
-  def show
-  end
+	def show
+	end
 
-  def new
+	def new
     @recipe = Recipe.new
-  end
+	end
 
 	def create
-		    @recipe = Recipe.new(recipe_params)
+		@recipe = Recipe.new(recipe_params)
 
 		if @recipe.save
 			redirect_to @recipe, notice: "Successfully created new recipe"
@@ -22,10 +22,10 @@ class RecipesController < ApplicationController
 		end
 	end
 
-  def edit
-  end
+	def edit
+	end
 
-  def update
+	def update
 		if @recipe.update(recipe_params)
 			redirect_to @recipe
 		else
@@ -33,20 +33,18 @@ class RecipesController < ApplicationController
 		end
 	end
 
-  def destroy
+	def destroy
 		@recipe.destroy
 		redirect_to root_path, notice: "Successfully deleted recipe"
 	end
 
-  private
+	private
 
-  def recipe_params
-		params.require(:recipe).permit(:title, :description, :image,
-        ingredients_attributes: [:id, :name, :_destroy],
-        directions_attributes: [:id, :step, :_destroy])
+	def recipe_params
+		params.require(:recipe).permit(:title, :description, :image, ingredients_attributes: [:id, :name, :recipe_id, :_destroy], directions_attributes: [:id, :step, :recipe_id, :_destroy])    
 	end
 
-  def find_recipe
-    @recipe = Recipe.find(params[:id])
-  end
+	def find_recipe
+		@recipe = Recipe.find(params[:id])
+	end
 end
